c1b8b59ff3cee4850ccddb37404a73cfb510ed2d
Bryan Sullivan <Bsully505@yahoo.com>
2021-03-08 12:34:10 -0500

    created a mockup version that would allow for qu to actually incorporate this system

--- src/StudentRecordReader.java
+++ src/StudentRecordReader.java
@@ -6,8 +6,16 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class StudentRecordReader {
+    int AM10;
+    int AM11;
+    int PM12;
+    int PM01;
+    int PM02;
+    int PM03;
+    int PM04;
 
     public StudentRecordReader(){
+        //what i could do is create an array instead so that i could traverse this easier
         int AM10 = 0;
         int AM11 = 0;
         int PM12 = 0;
@@ -27,9 +35,44 @@ public class StudentRecordReader {
         //This will be put into the system when a student taps card when getting tested
         int count = 0;
         String Line;
-        while((Line = in.nextLine()) != ""){
-            System.out.println(Line +" "+ count++);
+        while(in.hasNextLine() && (Line = in.nextLine()) != ""){
+            String Time = Line.split(":")[0];
+            //now this should have [hr as the string
+            Time = Time.substring(1);
+            int hour = Integer.parseInt(Time);
+            switch (hour){
+                case 10:
+                    AM10++;
+                    break;
+                case 11:
+                    AM11++;
+                    break;
+                case 12:
+                    PM12++;
+                    break;
+                case 01:
+                    PM01++;
+                    break;
+                case 02:
+                    PM02++;
+                    break;
+                case 03:
+                    PM03++;
+                    break;
+                case 04:
+                    PM04++;
+                    break;
+            }
 
+        }
+        int[] TimeRecords = {AM10,AM11,PM12,PM01,PM02,PM03,PM04};
+        String[] Times = {"10","11","12","01","02","03","04"};
+        int counterz = 0;
+        System.out.println("Hour Total");
+        for(int i :TimeRecords){
+            System.out.print(Times[counterz]+"   ");
+            System.out.println(i);
+            counterz++;
         }
 
 
