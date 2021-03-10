9047a50c4629f2099dea06a4cae92fdd6fa23c40
kkachmar <71847954+kkachmar@users.noreply.github.com>
2021-03-10 10:15:51 -0500

    creates toolbar

--- src/MainGui.java
+++ src/MainGui.java
@@ -1,10 +1,31 @@
 import java.util.Random;
+import javax.swing.*;
+import java.awt.*;
 
-public class MainGui {
-    public void run(){
+public class MainGui extends JFrame{
+    protected Toolbar toolbar;
+
+    MainGui(){
+        super("Wait Time");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,800);
+        setLayout(new BorderLayout());
+
+        toolbar = new Toolbar(this);
+        this.add(toolbar, BorderLayout.NORTH);
+
+        setVisible(true);
+
+    }
+
+
+    public void run(int selection){
         int[] monday = {30, 18, 55, 43, 10, 32, 20};
         int[] Wednesday = {40,33,65,42,35,45,15};
         NumberGenerator NG = new NumberGenerator(monday, Wednesday);
+        //need logic to pick monday or wednesday based on selection
+
+
         int numOfPeople = NG.GetSemiRandomWaitTime("Monday",-1);//test to run the method getSemi
         System.out.println("The amount of people is " + numOfPeople);
         int summer = 0;//summation counter
@@ -14,13 +35,11 @@ public class MainGui {
         summer = summer/11; // this is for the amount of tables to go to to get tested.
         int totalTime = summer;
         System.out.println("Your wait time is "+ (totalTime/60)+":"+ totalTime%60);
-
-
     }
 
     public static void main(String[] args){
        MainGui main = new MainGui();
-       main.run();
+       main.run(1);
 
     }
 }
